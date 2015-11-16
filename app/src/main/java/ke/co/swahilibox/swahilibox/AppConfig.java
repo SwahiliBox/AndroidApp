cc79a2b337a2a4939cf876e189287ef22b346e0b
Japheth7 <japhethobala@yahoo.com>
2015-11-16 18:15:03 -0500

    Did basic app ui, added helper classes, set up parse

--- app/src/main/java/ke/co/swahilibox/swahilibox/AppConfig.java
+++ app/src/main/java/ke/co/swahilibox/swahilibox/AppConfig.java
@@ -6,6 +6,8 @@ package ke.co.swahilibox.swahilibox;
 public class AppConfig {
 
     public static final String PARSE_CHANNEL = "SwahiliBox";
+
+    //aplication id and client_key for use in development mode
     public static final String PARSE_APPLICATION_ID = "1xFRMt4BUdhYXO0ucDWm1tUzdRv9Ut4u0vNQN7Ma";
     public static final String PARSE_CLIENT_KEY = "AbnShupSO0cRMBQIZ4E5KKqLGSlEIvcZK3BEG0Nc";
     public static final int NOTIFICATION_ID = 100;
